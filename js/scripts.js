c1badaa65c5e47669a45979b393484f14094596c
Dexter Rualo <dexterrualo@gmail.com>
2017-01-29 15:11:38 +0800

    updated timer object

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,57 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var Timer = function(){
+  var Timer = function(secs){
     var startTime = 0;
+    var duration = secs;
     var worker = undefined;
-    this.start = function(duration, process) {
+    var remaining = duration;
+    this.start = function(process) {
       if (worker == undefined) {
-        worker = setInterval(process, 1000);
-        startTime = Date.now();
-        setTimeout(function(){
-          worker = clearInterval(worker);
-        }, duration);
+        worker = setInterval(function(){
+          remaining -= 1;
+          process();
+          console.log(remaining);
+          if (remaining <= 0)
+            worker = clearInterval(worker);
+        }, 1000);
       }
     };
     this.stop = function() {
       if (worker){
         worker = clearInterval(worker);
-        startTime = 0;
       }
     };
-    this.getElapsed = function(){
-      return Date.now() - startTime;
+    this.setDuration = function(num) {
+      duration = remaining = num;
+    };
+    this.getDuration = function() {
+      return duration;
+    };
+    this.getRemaining = function() {
+      var min = Math.floor(remaining/60);
+      var sec = remaining % 60;
+      return (min > 9 ? min : '0' + min) + ':' + (sec > 9 ? sec : '0' + sec);
     };
   };
 //----------------------------------------------
-  var workTimer = new Timer();
+  var workTimer = new Timer(1500);
+  document.getElementById('timer-display').innerHTML = workTimer.getRemaining();
   document.getElementById('start').onclick = function(){
-    workTimer.start(10000, function(){
-      console.log(workTimer.getElapsed());
+    workTimer.start(function(){
+      document.getElementById('timer-display').innerHTML = workTimer.getRemaining();
+      console.log(workTimer.getRemaining());
     });
   };
   document.getElementById('stop').onclick = function(){
     workTimer.stop();
+  };
+  document.getElementById('work-minus').onclick = function(){
+    workTimer.setDuration(workTimer.getDuration() - 60);
+    document.getElementById('work-minutes').innerHTML = workTimer.getDuration()/60;
+    document.getElementById('timer-display').innerHTML = workTimer.getRemaining();
+  };
+  document.getElementById('work-plus').onclick = function(){
+    workTimer.setDuration(workTimer.getDuration() + 60);
+    document.getElementById('work-minutes').innerHTML = workTimer.getDuration()/60;
+    document.getElementById('timer-display').innerHTML = workTimer.getRemaining();
   };
 });
